944f9ef2810d6ccf80f51450504688b74ce6066d
leejohnnn <lee.joh@northeastern.edu>
2024-09-19 16:57:36 -0400

    implemented appwrite, authform, sentry

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer'
 
 const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
@@ -50,7 +51,9 @@ const Sidebar = ({ user }: SidebarProps) => {
         USER
       </nav>
 
-        FOOTER
+        <Footer> user={user}
+
+        </Footer>
     </section>
   )
 }
